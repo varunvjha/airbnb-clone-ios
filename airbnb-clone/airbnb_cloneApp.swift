06bbb0e27f19781ca6ffa5fd4e5afd826456ee89
Varun Jha <varun.jha@spense.money>
2023-10-25 10:11:49 +0530

    Initial Commit

---
+++ airbnb-clone/airbnb_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  airbnb_cloneApp.swift
+//  airbnb-clone
+//
+//  Created by Varun on 25/10/23.
+//
+
+import SwiftUI
+
+@main
+struct airbnb_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
